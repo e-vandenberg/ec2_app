3f0801a5c94c0c9c6f74f6f26e4086de1fb0791e
e-vandenberg <erik.vandenberg@ucc.on.ca>
2019-06-13 10:10:45 -0400

    comment

--- contact_db.py
+++ contact_db.py
@@ -2,6 +2,7 @@ import _mysql
 
 class Database:
         def __init__(self, endp, user, pwd, db):
+                # contact the RDS Database
                 self.db=_mysql.connect(endp, user, pwd, db)
 
         def get_age(self, name):
