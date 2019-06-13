b7086b7b81f68ca21bfc90efeae08b6999f15ea0
e-vandenberg <erik.vandenberg@ucc.on.ca>
2019-06-13 14:55:52 -0400

    add users

--- contact_db.py
+++ contact_db.py
@@ -8,5 +8,10 @@ class Database:
         def get_age(self, name):
                 self.db.query("""SELECT age FROM accounts WHERE name = '%s'""" % (name))
                 result = self.db.use_result()
-                return result.fetch_row()[0][0]
                 self.db.close()
+                return result.fetch_row()[0][0]
+
+        def new_user(self, name, age):
+                self.db.query("""INSERT INTO accounts (name, age) VALUES ('%s', '%s')""" % (name, age))
+                self.db.close()
+                return "new account created"
--- main.py
+++ main.py
@@ -7,11 +7,23 @@ app = Flask(__name__)
 def home():
     return render_template('form.html')
 
+@app.route('/new_user')
+def page():
+    return render_template('user_form.html')
+
 @app.route('/', methods=['POST'])
-def my_form_post():
+def form_post():
     text = request.form['text']
     d = contact_db.Database(endpoint, user, pass, db)
     processed_text = d.get_age(text.upper())
+    return processed_text
+
+@app.route('/new_user', methods=['POST'])
+def create_account():
+    name = request.form['name']
+    age = request.form['age']
+    d = contact_db.Database(endpoint, user, pass, db)
+    processed_text = d.new_user(name.upper(), age.upper())
     return processed_text
 
 if __name__ == "__main__":
